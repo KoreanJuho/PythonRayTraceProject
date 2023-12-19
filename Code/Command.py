821cf3303949fc26e91623e5f22910f1fe430a87
Juho <jhle01394@gmail.com>
2023-12-19 09:22:53 +0900

    Deleted

--- Code/Command.py
+++ Code/Command.py
@@ -1,6 +1,7 @@
 import Analysis as an
 
 ad = an.Diagnostics("e")
+
 # ad.fielsago()
 # ad.rimgo()
 ad.SpotDiagram(type="square")
--- Code/Optimize.py
+++
@@ -1,8 +0,0 @@
-import Analysis as an
-
-ad = an.Diagnostics("e")
-
-ad.fielsago()
-ad.rimgo()
-spotsize = ad.SpotDiagram(type="square", graph=True)
-ad.Viego()
